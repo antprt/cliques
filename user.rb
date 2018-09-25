8a0287a4f7642e00205aa83cee7745f1e6306307
antprt <antoniopr.apr@gmail.com>
2018-09-25 17:47:44 +0200

    Extended user functionality: Search between array of user objects by twitter id Setting the full_connected attribute through an array of users

--- user.rb
+++ user.rb
@@ -1,9 +1,34 @@
 class User
-  attr_accessor :name, :twitter_id, :twitter_friends_ids, :github_id, :github_organizations_ids
+  attr_accessor :name, :twitter_id, :twitter_friends_ids, :github_id, :github_organizations_ids, :full_connected_users
 
   def initialize(name)
     @name = name
     @twitter_friends_ids = []
     @github_organizations_ids = []
+    @full_connected_users = []
+  end
+
+  def self.find_by_twitter_id(twitter_id, users)
+    user_find = nil
+    users.each do |user|
+      if user.twitter_id == twitter_id
+        user_find = user
+        break
+      end
+    end
+    return user_find
+  end
+
+  def self.set_full_connected(users)
+    users.each do |user|
+      user.twitter_friends_ids.each do |twitter_id|
+        user_friend = User.find_by_twitter_id(twitter_id, users)
+        unless user_friend.nil?
+          if user_friend.twitter_friends_ids.include?(user.twitter_id) && (user_friend.github_organizations_ids & user.github_organizations_ids).any?
+            user.full_connected_users.push(user_friend)
+          end 
+        end
+      end
+    end
   end
 end
--- user_test.rb
+++ user_test.rb
@@ -5,4 +5,43 @@ class UserTest < Test::Unit::TestCase
   def test_creation 
     assert_equal "Nombre", User.new("Nombre").name
   end
+
+  def test_find_by_twitter_id_found
+    users = set_array_of_users
+    assert_equal users[2], User.find_by_twitter_id(2, users)
+  end
+
+  def test_find_by_twitter_id_not_found
+    users = set_array_of_users
+    assert_equal nil, User.find_by_twitter_id(3, users)
+  end
+
+  def test_set_full_connected
+    users = set_array_of_users
+    User.set_full_connected(users)
+    puts users[0].full_connected_users
+    puts users[2].full_connected_users
+    assert_equal [users[2]], users[0].full_connected_users
+  end
+
+  private
+  def set_array_of_users
+    users = [User.new("A"), User.new("B"), User.new("C")]
+    users[0].twitter_id = 0
+    users[0].github_id = 10
+    users[0].github_organizations_ids = [30, 70, 80]
+    users[0].twitter_friends_ids = [2, 67]
+
+    users[1].twitter_id = 1
+    users[1].github_id = 20
+    users[1].github_organizations_ids = []
+    users[1].twitter_friends_ids = [100, 500, 600]
+
+    users[2].twitter_id = 2
+    users[2].github_id = 30
+    users[2].github_organizations_ids = [30, 1000]
+    users[2].twitter_friends_ids = [0, 6500]
+
+    return users
+  end
 end
