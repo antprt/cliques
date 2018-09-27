ed2af510f0e015f7c0ba9bb4e68b8cd2431159e4
antprt <antoniopr.apr@gmail.com>
2018-09-27 18:35:26 +0200

    main thread updated with outputs translate to english

--- lib/main.rb
+++ lib/main.rb
@@ -9,54 +9,33 @@ class Main
   require_relative "clique"
 
   def self.main
-    puts "Leyendo fichero de entrada"
+    puts "Reading input file"
     file_content = FileHandlerInput.new.read_file
     usernames = TextHandler.new(file_content).parse_to_array_unique
 
-    puts "Generando usuarios: "
+    puts "Generating users: "
     users = []
     usernames.each do |username|
       users << User.new(username)
-      puts "Usuario #{username} generado"
+      puts "User #{username} generate"
     end
-=begin
-    puts "Conectando a Twitter y Github"
+
+    puts "Conecting to Twitter and Github"
     users.each do |user|
-      puts "Obteniendo información de Twitter y Github para el usuario #{user.name}"
+      puts "Getting information of Twitter and Github for the user #{user.name}. This operation can take a long time due to the restrictions of twitter requests."
       user.twitter_id = TwitterHandler.get_user_id(user.name)
       user.twitter_friends_ids = TwitterHandler.get_user_friends_ids(user.name)
       user.github_id = GithubHandler.get_user_id(user.name)
       user.github_organizations_ids = GithubHandler.get_organizations_ids(user.name)
     end
-=end
-    
-    users[0].twitter_id = 4564353
-    users[0].twitter_friends_ids = [55455, 77778]
-    users[0].github_id = 888984
-    users[0].github_organizations_ids = [985]
 
-    users[1].twitter_id = 677676
-    users[1].twitter_friends_ids = [4564353, 55455]
-    users[1].github_id = 888984
-    users[1].github_organizations_ids = [985]
-
-    users[2].twitter_id = 55455
-    users[2].twitter_friends_ids = [4564353, 77778, 677676]
-    users[2].github_id = 888984
-    users[2].github_organizations_ids = [985, 556, 8744]
-
-    users[3].twitter_id = 77778
-    users[3].twitter_friends_ids = [4564353, 55455]
-    users[3].github_id = 888984
-    users[3].github_organizations_ids = [985, 556]
-
-    puts "Obteniendo usuarios completamente conectados y los cliques correspondientes. Esta operación puede tardar unos minutos."
+    puts "Getting full connected users and cliques. This operation can take a few minutes."
     users_cliques = Clique::find_cliques(User.set_full_connected(users))
 
-    puts "Generando fichero de salida"
+    puts "Generating ouput file"
     file_output = FileHandlerOutput.new
     file_output.write_file(Clique::order_cliques_alphabetically(users_cliques))
-    puts "Fichero de salida generado"
+    puts "Output file generated"
   end 
 
   begin
